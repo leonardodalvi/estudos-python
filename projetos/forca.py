f459c05da69d68c4a1795e6568499495c8f2ff37
Leonardo Dalvi Alvarenga <leonardodalvi@outlook.com>
2020-12-17 10:21:58 -0300

    inclui vidas forca

--- projetos/forca.py
+++ projetos/forca.py
@@ -14,13 +14,14 @@ def forca():
     letras_palavra = set(palavra) # letras na palavra
     alfabeto = set(string.ascii_uppercase)
     letras_usadas = set() # as que o jogador tentou adivinhar
-    
+    vidas = 6
+
     # entrada do jogador
-    while len(letras_palavra) > 0:
+    while len(letras_palavra) > 0 and vidas > 0:
     
         # letras usadas
         # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-        print('Você já usou estas letras: ', ' '.join(letras_usadas))
+        print('Você possui', vidas,'vidas restantes e já usou estas letras: ', ' '.join(letras_usadas))
 
         # estado da palavra (ex. P A L _ V R A)
         lista_palavra = [letra if letra in letras_usadas else '_' for letra in palavra]
@@ -31,6 +32,9 @@ def forca():
             letras_usadas.add(tentativa)
             if tentativa in letras_palavra:
                 letras_palavra.remove(tentativa)
+            else:
+                vidas = vidas - 1 # tira uma vida se errar
+                print('A palavra não contém esta letra.')
         
         elif tentativa in letras_usadas:
             print('Você já escolheu essa letra. Por favor, tente outra.')
@@ -38,7 +42,10 @@ def forca():
         else:
             print('Caractere inválido. Por favor, tente novamente.')
     
-    # quando len(letras_palavra) == 0
-    print('Parabéns, você acertou a palavra: ', palavra)
+    # quando len(letras_palavra) == 0 ou quando vidas == 0
+    if vidas == 0:
+        print('Você morreu, desculpe. A palavra era', palavra)
+    else:
+        print('Parabéns, você acertou a palavra: ', palavra)
 
 forca()
